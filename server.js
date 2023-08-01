9e760f5faff73bc83aad9a2eba7aedcaf3c27f24
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-01 19:07:48 +0300

    creating env file having PORT=5000 no semicolon

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // creating express 
 const express = require('express');
+// create dotenv config
+const dotenv= require('dotenv').config(); 
 
 //creating the express app
 const app = express();
 
 //creating a port number
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // creating the express app with the port number to listen on
 app.listen(port,() => {
