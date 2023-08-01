8d664be34196f687268d53e00d93d27f84cf7e4c
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-01 18:43:34 +0300

    installing nodemon and config on the json file and server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 // creating express 
 const express = require('express');
+
 //creating the express app
 const app = express();
+
+//creating a port number
+const port = 4000;
+
+// creating the express app with the port number to listen on
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`);
+});
 
