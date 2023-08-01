10905ab006c7751a678b949959b60114b5983c2a
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-01 20:51:47 +0300

    all routing define on routes folder

--- routes/contactsRouts.js
+++ routes/contactsRouts.js
@@ -5,6 +5,20 @@ const routes = express.Router();
     res.status(200).json({message:"Get all contacts"});
 });
 
+routes.get("/api/contacts/:id", (req, res) => {
+res.status(200).json({message:`Get contact for ${req.params.id}`});
+});
 
+routes.post("/api/contacts", (req, res) => {
+    res.status(200).json({message:"Create contact"});
+});
+
+routes.put("/api/contacts/:id", (req, res) => {
+    res.status(200).json({message:`Update contact for ${req.params.id}`});
+});
+
+routes.delete("/api/contacts/:id", (req, res) => {
+    res.status(200).json({message:`Delete contact for ${req.params.id}`});
+});
 // exporting 
 module.exports=routes;
