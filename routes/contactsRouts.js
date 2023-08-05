9f9f2b987160f98ccefff3648dc2da2819dbb826
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-05 15:53:28 +0300

    creating both get by ID and all contacts

--- routes/contactsRouts.js
+++ routes/contactsRouts.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const routes = express.Router();
-const {getContacts}= require('../controller/contactController');
+const {getContacts,getContact}= require('../controller/contactController');
  // defining all routes for contacts 
  routes.get("/",getContacts);
 
-routes.get("/:id", (req, res) => {
-res.status(200).json({message:`Get contact for ${req.params.id}`});
-});
+routes.get("/:id", getContact);
 
-routes.post("/", (req, res) => {
-    res.status(200).json({message:"Create contact"});
-});
+routes.post("/",);
 
 routes.put("/:id", (req, res) => {
     res.status(200).json({message:`Update contact for ${req.params.id}`});
