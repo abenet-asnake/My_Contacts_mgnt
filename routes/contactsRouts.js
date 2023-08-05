6a600e219148fcb44082ee8d9d82c4e6b940e257
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-05 15:47:01 +0300

    creating all routes controlled in controller

--- routes/contactsRouts.js
+++ routes/contactsRouts.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const routes = express.Router();
+const {getContacts}= require('../controller/contactController');
  // defining all routes for contacts 
- routes.get("/", (req, res) => {
-    res.status(200).json({message:"Get all contacts"});
-});
+ routes.get("/",getContacts);
 
 routes.get("/:id", (req, res) => {
 res.status(200).json({message:`Get contact for ${req.params.id}`});
