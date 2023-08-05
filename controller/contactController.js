2da26ffb87a98606fc5b1944473350717f06fb7a
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-05 15:54:03 +0300

    both get and post method to create new contact

--- controller/contactController.js
+++ controller/contactController.js
@@ -4,4 +4,20 @@
 const getContacts=(req, res) => {
     res.status(200).json({message:"Get all contacts"});
 };
-module.exports = {getContacts};
+
+//@disc getContact get single contact by id 
+//@routes GET /api/contacts
+//@access public
+const getContact = (req, res) => {
+    res.status(200).json({message:`Get contact for ${req.params.id}`});
+    };
+
+//@disc getContact create a new contact
+//@routes POST /api/contacts
+//@access public
+const createContacts =  (req, res) => {
+    res.status(200).json({message:"Create contact"});
+};
+module.exports = {getContacts,
+                  getContact,
+                  createContacts};
