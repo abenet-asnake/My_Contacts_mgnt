eaf7b7229cf0dfb6c9e9e27bbbd8ac89df0c212c
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-06 19:30:47 +0300

    let define constant.js file

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -5,23 +5,25 @@ const statusCode =res.statusCode ? res.statusCode : 500 ;
 //creating switch case define the status code 
 switch (statusCode) {
     case 400:
+        res.json({tittle:"Validation Error", stackTrace: error.stack});
         break ;
     case 401:
+        res.json({tittle:"Unauthorized", stackTrace: error.stack});
         break ;
     case 403:
+        res.json({tittle:"Forbidden", stackTrace: error.stack});
         break ;
     case 404:
+        res.json({tittle:"Not Found", stackTrace: error.stack});
         break ;
     case 500:
+        res.json({tittle:"Server Error", stackTrace: error.stack});
         break ;
     default:
         console.log("No Error It is Fine");
         break ;
 }
-res.json({tittle:"Validation Error", stackTrace: error.stack});
-res.json({tittle:"Unauthorized", stackTrace: error.stack});
-res.json({tittle:"Forbidden", stackTrace: error.stack});
-res.json({tittle:"Not Found", stackTrace: error.stack});
+
 };
 
 module.exports =errorHandler;
