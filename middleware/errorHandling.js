57b1772671c5543f201ed556428017e11ba32a4b
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-06 20:11:41 +0300

    constans

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,22 +1,24 @@
+// using the constant.js
+const { constants } = require('./constant');
 // we are passing four parameters for middleware 
 const errorHandler = (error,req,res,next) => {
 // we have define the status code 
 const statusCode =res.statusCode ? res.statusCode : 500 ;
 //creating switch case define the status code 
 switch (statusCode) {
-    case 400:
+    case constants.VALIDATION_ERROR:
         res.json({tittle:"Validation Error", stackTrace: error.stack});
         break ;
-    case 401:
+    case constants.UNAUTHORIZED:
         res.json({tittle:"Unauthorized", stackTrace: error.stack});
         break ;
-    case 403:
+    case constants.FORBIDDEN:
         res.json({tittle:"Forbidden", stackTrace: error.stack});
         break ;
-    case 404:
+    case constants.NOT_FOUND:
         res.json({tittle:"Not Found", stackTrace: error.stack});
         break ;
-    case 500:
+    case constants.SERVER_ERROR:
         res.json({tittle:"Server Error", stackTrace: error.stack});
         break ;
     default:
