427a0fcf5bb2f21a52346565fe7ad0c634a97bb3
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-06 18:41:36 +0300

    message of the error and statck trace object in form of JSON

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,6 +1,8 @@
 // we are passing four parameters for middleware 
 const errorHandler = (error,req,res,next) => {
-
+// we have define the status code 
+const statusCode =res.statusCode ? res.statusCode : 500 ;
+res.json({message: error.message,stackTrace:error.stack});
 };
 
 module.exports =errorHandler;
