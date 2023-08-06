5719c223692c9ddd799f3017a99d56e9f836c089
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-06 19:22:31 +0300

    defining the status code

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -2,7 +2,10 @@
 const errorHandler = (error,req,res,next) => {
 // we have define the status code 
 const statusCode =res.statusCode ? res.statusCode : 500 ;
-res.json({message: error.message, stackTrace: error.stack});
+res.json({tittle:"Validation Error", stackTrace: error.stack});
+res.json({tittle:"Unauthorized", stackTrace: error.stack});
+res.json({tittle:"Forbidden", stackTrace: error.stack});
+res.json({tittle:"Not Found", stackTrace: error.stack});
 };
 
 module.exports =errorHandler;
