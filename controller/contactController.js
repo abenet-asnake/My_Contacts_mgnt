b0c4838d66d8bede380b6e20db4eacbc8973a102
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-06 17:42:10 +0300

    we are using middle ware to parse the joson body

--- controller/contactController.js
+++ controller/contactController.js
@@ -16,6 +16,7 @@ const getContact = (req, res) => {
 //@routes POST /api/contacts
 //@access public
 const createContacts =  (req, res) => {
+    console.log("The create New Contact is",req.body);
     res.status(200).json({message:"Create contact"});
 };
 
