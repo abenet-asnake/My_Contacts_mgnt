3bcf13e569fdacd96acc4f1ec016a5892ceafbfe
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-06 19:28:25 +0300

    defining ALL THE status code in the swich case we can create constant.js file

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -2,6 +2,22 @@
 const errorHandler = (error,req,res,next) => {
 // we have define the status code 
 const statusCode =res.statusCode ? res.statusCode : 500 ;
+//creating switch case define the status code 
+switch (statusCode) {
+    case 400:
+        break ;
+    case 401:
+        break ;
+    case 403:
+        break ;
+    case 404:
+        break ;
+    case 500:
+        break ;
+    default:
+        console.log("No Error It is Fine");
+        break ;
+}
 res.json({tittle:"Validation Error", stackTrace: error.stack});
 res.json({tittle:"Unauthorized", stackTrace: error.stack});
 res.json({tittle:"Forbidden", stackTrace: error.stack});
