6f4dfef94b1f4d8751d13219644c409b916bacd7
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-10 19:02:53 +0300

    console log to display host and database name

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -6,11 +6,12 @@ const db_connect= async () => {
     try {
         //there is await db_connect to connect to
         const connect = await mongoose.connect(process.env.DB_STRING);
-
+        // console log to display host and database name 
+        console.log("The Database is connected: ", connect.connection.host, connect.connection.name);
     } catch (error) {
             console.log(error);
             process.exit(1);
     }
 };
-// exporting this conection 
+// exporting this Connection  
 module.exports =db_connect;
