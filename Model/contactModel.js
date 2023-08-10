f6570530099f6726266571d46cae835cc08e7065
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-10 19:45:41 +0300

    creating contact schema

---
+++ Model/contactModel.js
@@ -0,0 +1,17 @@
+// initialized mongoose package 
+const mongoose = require('mongoose');
+// creating mongoose Schema object data type for mongoose
+const contactSchema=mongoose.Schema({
+    fullName:{
+        type:String,
+        require:[true,"Please add a full name"],
+    },
+    email:{
+        type:String,
+        require:[true,"Please add a full name"],           
+    },
+    phone:{
+        type:String,
+        require:[true,"Please add a full name"],
+
+},})
