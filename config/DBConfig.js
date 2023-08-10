44b2e14d89f15d820e19410ecac80bc0b802d275
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-10 10:32:14 +0300

    creating arrow function that connect the database

---
+++ config/DBConfig.js
@@ -0,0 +1,13 @@
+// loading mongoose package
+const mongoose= require('mongoose');
+// let create arrow functions for db_connect 
+
+const db_connect= async () => { 
+    try {
+        //there is await db_connect to connect to
+        
+
+    } catch (error) {
+
+    }
+};
