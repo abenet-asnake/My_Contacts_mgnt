fede19a0b046fdc86f37768b287608b49c244f67
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-11 17:54:36 +0300

    creating post API routes for registration user status

--- routes/userRouts.js
+++ routes/userRouts.js
@@ -1,4 +1,17 @@
 const express= require('express');
 const router= express.Router();
+// creating post API routes for registration 
+router.post("/register", (req, res) => {
+       res.json({Message: 'User Successfully Registered'});
+});
 
+// login users
+router.post("/login", (req, res) => {
+    res.json({Message: 'User Successfully Login'});
+});
+
+// status of the user 
+router.get("/status", (req, res) => {
+    res.json({Message: 'User Status'});
+});
 module.exports = router;
