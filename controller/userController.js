06c74b9a72356279f979e666abb5b9ee95fb56a1
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-11 20:05:00 +0300

    checking all filed is mandatory and checking the avaliblity of user by their email address

--- controller/userController.js
+++ controller/userController.js
@@ -7,7 +7,26 @@ const user_model = require('../Model/userModel');
 //@routes POST /api/users/register
 //@access public
 const registerUsers = asyncHandler (async (req, res) => {
-    res.json({Message: 'User Successfully Registered'})});
+    //destructure registerUsers
+    const {userName,email,password}= req.body;
+    if (!userName || !email || !password) {
+         res.status(404);
+         throw new Error ("All Filed are required to register");
+    }
+    const user_Available = await user_model.findOne({email});
+    if(user_Available) {
+        res.status(400);
+        throw new Error ("Email Address already Exists");
+    }
+    // the password is RAW so that we have to hash the password
+    //so we have to install library hash npm install bycrypt
+     
+    const createUser = await user_model.create({
+        userName,
+        email,
+        password
+    })
+    res.json(createUser)});
 
 //@disc loginUsers 
 //@routes POST /api/users/login 
