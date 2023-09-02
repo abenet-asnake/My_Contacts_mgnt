03a4829e2623c7ea9d2ce9c9ed1663e52fce2bbf
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-02 15:11:00 +0300

    decode

--- middleware/tokenValidateHandler.js
+++ middleware/tokenValidateHandler.js
@@ -1,12 +1,17 @@
 const asyncHandler = require('express-async-handler');
 const jwt= require('jsonwebtoken');
+
 const tokenHandler= asyncHandler(async(req, res, next)=>{
-    let token=req.headers.Authorization;
-    jwt.sign(token,process.env.ACCESS_TOKE_SECRET_KEY,(err,decoded)=>{
+  let token;
+  let authHeader = req.headers.Authorization || req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")){
+    token = authHeader.split(" ")[1];
+    jwt.verify(token,process.env.ACCESS_TOKE_SECRET_KEY,(err,decoded)=>{
         if (err){
             res.status(401);
             throw new Error ('User not authenticated');
         }
+        console.log(decoded);
         req.thisUser=decoded.thisUser;
         next();
     });
@@ -14,6 +19,7 @@ const tokenHandler= asyncHandler(async(req, res, next)=>{
         res.status(401);
         throw new Error ('token is required');
     }
+  }
 });
 
 module.exports =tokenHandler;
